55bbef9eb4b540a6f59cda308d43571291fe59b5
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-03-21 02:00:01 +0900

    feat : Font 추가 및 Font extension 구현

---
+++ RankIn/RankIn/extension/UIFont+Pretendard.swift
@@ -0,0 +1,30 @@
+//
+//  FontManager.swift
+//  RankIn
+//
+//  Created by 조성민 on 3/21/24.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    enum Pretendard: String {
+        
+        case regular = "Pretendard-Regular"
+        case thin = "Pretendard-Thin"
+        case extraLight = "Pretendard-ExtraLight"
+        case light = "Pretendard-Light"
+        case medium = "Pretendard-Medium"
+        case semiBold = "Pretendard-SemiBold"
+        case bold = "Pretendard-Bold"
+        case extraBold = "Pretendard-ExtraBold"
+        case black = "Pretendard-Black"
+        
+    }
+    
+    func pretendard(type: Pretendard, size: CGFloat) -> UIFont {
+        return UIFont(name: type.rawValue, size: size)!
+    }
+    
+}
