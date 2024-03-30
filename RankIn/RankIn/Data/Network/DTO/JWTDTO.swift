643374169bcc1d6cb97d1e04fa3ef31b21c8d5a2
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-03-31 01:53:20 +0900

    chore : Apple 로그인 결과애서 필요한 DTO와 모델 작성

---
+++ RankIn/RankIn/Data/Network/DTO/JWTDTO.swift
@@ -0,0 +1,15 @@
+//
+//  JWTDTO.swift
+//  RankIn
+//
+//  Created by 조성민 on 3/31/24.
+//
+
+import Foundation
+
+struct JWTDTO {
+    
+    let accessToken: String
+    let refreshToken: String
+    
+}
