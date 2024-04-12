1fed8d75e203c9715b93d3a75fde3c8f3d7f01bb
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-04-13 04:19:25 +0900

    feat : 세종대 로그인 DTO 작성

---
+++ RankIn/RankIn/Data/Network/DTO/SejongLoginInfoDTO.swift
@@ -0,0 +1,15 @@
+//
+//  SejongLoginInfoDTO.swift
+//  RankIn
+//
+//  Created by 조성민 on 4/13/24.
+//
+
+import Foundation
+
+struct SejongLoginInfoDTO: Encodable {
+    
+    let id: String
+    let pw: String
+    
+}
---
+++ RankIn/RankIn/Data/Network/DTO/SejongLoginResultDTO.swift
@@ -0,0 +1,15 @@
+//
+//  SejongLoginResultDTO.swift
+//  RankIn
+//
+//  Created by 조성민 on 4/13/24.
+//
+
+import Foundation
+
+struct SejongLoginResultDTO: Decodable {
+    
+    // TODO: isAuthorized로 수정
+    let isSejong: Bool
+    
+}
