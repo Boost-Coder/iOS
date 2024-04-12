4e840e76618f4d71a78152e1e6bc54843ab48565
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-04-13 02:14:12 +0900

    feat : LoginResultDTO 작성

---
+++ RankIn/RankIn/Data/Network/DTO/LoginResultDTO.swift
@@ -0,0 +1,16 @@
+//
+//  LoginResultDTO.swift
+//  RankIn
+//
+//  Created by 조성민 on 4/13/24.
+//
+
+import Foundation
+
+struct LoginResultDTO: Decodable {
+    
+    let accessToken: String
+    let refreshToken: String
+    let isMember: Bool
+    
+}
