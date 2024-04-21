c4be6eb9a93c36995a47ffcd60130f54a277560a
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-04-21 20:39:17 +0900

    feat : GitHub 실패시 Toast

--- RankIn/RankIn/extension/UIViewController+Toast.swift
+++ RankIn/RankIn/extension/UIViewController+Toast.swift
@@ -25,6 +25,7 @@ extension UIViewController {
         case noGradeInput
         case invalidGradeInput
         case sejongLoginFailed
+        case gitHubAuthorizationFailed
         
     }
     
@@ -40,6 +41,8 @@ extension UIViewController {
             self.view.makeToast("소수점 두자리 까지의 형식으로 학점을 입력하세요\n예시 : 3.94")
         case .sejongLoginFailed:
             self.view.makeToast("로그인에 실패했습니다\n아이디와 비밀번호를 확인해주세요")
+        case .gitHubAuthorizationFailed:
+            self.view.makeToast("GitHub 인증에 실패했습니다")
         }
     }
     
