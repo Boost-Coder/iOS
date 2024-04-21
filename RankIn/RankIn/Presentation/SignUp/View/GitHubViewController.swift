7d63f90ce4ba74888812ca986af9f3084bde9a80
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-04-21 20:41:04 +0900

    feat : GitHubViewController 구현

---
+++ RankIn/RankIn/Presentation/SignUp/View/GitHubViewController.swift
@@ -0,0 +1,159 @@
+//
+//  GitHubViewController.swift
+//  RankIn
+//
+//  Created by 조성민 on 4/18/24.
+//
+
+import UIKit
+import RxSwift
+import RxRelay
+
+final class GitHubViewController: UIViewController {
+    
+    private let disposeBag = DisposeBag()
+    
+    // MARK: Input
+    let gitHubAuthorizationSuccess: PublishRelay<String>
+    
+    private lazy var gitHubButton: UIButton = {
+        var attributedTitle = AttributedString("GitHub 인증")
+        attributedTitle.font = UIFont.pretendard(type: .medium, size: 20)
+        
+        var configuration = UIButton.Configuration.filled()
+        configuration.baseBackgroundColor = .gitHub
+        configuration.baseForegroundColor = .white
+        configuration.image = UIImage.gitHub
+        configuration.imagePlacement = .leading
+        configuration.attributedTitle = attributedTitle
+        configuration.titleAlignment = .center
+        configuration.imagePadding = 15
+        
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.configuration = configuration
+        
+        return button
+    }()
+    
+    private let skipButton: UIButton = {
+        var attributeTitle = AttributedString("건너뛰기")
+        attributeTitle.font = UIFont.pretendard(type: .regular, size: 15)
+        
+        var configuration = UIButton.Configuration.plain()
+        configuration.attributedTitle = attributeTitle
+        configuration.baseForegroundColor = .systemGray2
+        
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.configuration = configuration
+        
+        return button
+    }()
+    
+    private let viewModel: GitHubViewModel
+    private let mainTabBarController: UITabBarController
+    
+    init(
+        gitHubViewModel: GitHubViewModel,
+        gitHubAuthorizationSuccess: PublishRelay<String>,
+        mainTabBarController: UITabBarController
+    ) {
+        self.viewModel = gitHubViewModel
+        self.gitHubAuthorizationSuccess = gitHubAuthorizationSuccess
+        self.mainTabBarController = mainTabBarController
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        bind()
+        react()
+    }
+    
+}
+
+private extension GitHubViewController {
+    
+    func setUI() {
+        view.backgroundColor = .systemGray6
+        
+        setHierarchy()
+        setConstraints()
+    }
+    
+    func setHierarchy() {
+        view.addSubview(gitHubButton)
+        view.addSubview(skipButton)
+    }
+    
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            gitHubButton.widthAnchor.constraint(equalToConstant: 285),
+            gitHubButton.heightAnchor.constraint(equalToConstant: 44),
+            gitHubButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            gitHubButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 258)
+        ])
+        
+        NSLayoutConstraint.activate([
+            skipButton.centerXAnchor.constraint(equalTo: gitHubButton.centerXAnchor),
+            skipButton.topAnchor.constraint(equalTo: gitHubButton.bottomAnchor, constant: 5)
+        ])
+    }
+    
+    func bind() {
+        let input = GitHubViewModelInput(
+            gitHubAuthorizationSuccess: gitHubAuthorizationSuccess
+        )
+        
+        let output = viewModel.transform(input: input)
+        
+        output.errorPublisher
+            .bind { error in
+                self.presentErrorToast(error: error)
+            }
+            .disposed(by: disposeBag)
+        
+        output.gitHubAuthorizationRegisterFailure
+            .bind { _ in
+                self.presentToast(toastCase: .gitHubAuthorizationFailed)
+            }
+            .disposed(by: disposeBag)
+        
+        output.gitHubAuthorizationRegisterSuccess
+            .bind { _ in
+                // TODO: 다음 VC present
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    func react() {
+        gitHubButton.rx
+            .tap
+            .bind { _ in
+                // TODO: guard 탈출 대응
+                guard let clientID = Bundle.main.object(
+                    forInfoDictionaryKey: "GITHUB_CLIENT_ID"
+                ) as? String else { return }
+                let urlString = "https://github.com/login/oauth/authorize?client_id=\(clientID)"
+                guard let url = URL(string: urlString) else { return }
+                UIApplication.shared.open(url)
+            }
+            .disposed(by: disposeBag)
+        
+        skipButton.rx
+            .tap
+            .bind { _ in
+                // TODO: 다음 VC 띄우기
+            }
+            .disposed(by: disposeBag)
+    }
+    
+}
