cac0364117ec3c6f9bcc74b069cbb4cc2e7ee6c0
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-05-22 23:42:21 +0900

    feat : UITableViewCell Identifier extension 작성

---
+++ RankIn/RankIn/extension/UITableViewCell+Identifier.swift
@@ -0,0 +1,16 @@
+//
+//  UITableViewCell+Identifier.swift
+//  RankIn
+//
+//  Created by 조성민 on 5/22/24.
+//
+
+import UIKit
+
+extension UITableViewCell {
+
+    static var identifier: String {
+        return String(describing: self)
+    }
+
+}
